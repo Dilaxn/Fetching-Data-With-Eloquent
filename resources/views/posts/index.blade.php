d64a0fee0ad1d0ed810602ca1a7fa955a6a91176
Dilaxn <dilaxn@gmail.com>
2021-05-10 23:49:17 +0530

    push and retrive the data and listed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     <h1>Posts</h1>
+    @if (count($posts)>0)
+    <ul>
+       @foreach ($posts as $post)
+           <p>{{$post -> title}}</p>
+       @endforeach
+    </ul>
+   @else 
+   <p>No posts found</p>
+    @endif
 @endsection
