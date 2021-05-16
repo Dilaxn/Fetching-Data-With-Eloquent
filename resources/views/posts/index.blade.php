37eeb64e475c1e4933c4965abdf9c78c08053991
Dilaxn <dilaxn@gmail.com>
2021-05-16 19:58:31 +0530

    href and used show method in controller

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
     @if (count($posts)>0)
     <ul>
        @foreach ($posts as $post)
-           <p><a href="/posts/{{$post->id}}">{{$post -> title}}</a></p>
+           <p><a href="http://localhost/lsapp/public/posts/{{$post->id}}">{{$post -> title}}</a></p>
        @endforeach
     </ul>
    @else 
